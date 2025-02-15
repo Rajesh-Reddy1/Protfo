0946c22a004912897e730263488aabe622530a6f
Rajesh-Reddy1 <mrrajeshreddy1@gmail.com>
2025-02-15 15:31:22 +0530

    title-image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,38 @@
-import { Inter } from 'next/font/google'
-import { cn } from '@/lib/utils'
-import './globals.css'
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const fontHeading = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-heading',
-})
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-heading",
+});
 
 const fontBody = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-body',
-})
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-body",
+});
 
-export default function Layout({ children}:any) {
+export default function Layout({ children }: any) {
   return (
     <html lang="en">
       <title>Protfolio</title>
-      <body 
-        className={cn(
-          'antialiased',
-          fontHeading.variable,
-          fontBody.variable
-        )}
-      >
+      <link
+        rel="icon"
+        href="https://cdn-icons-png.flaticon.com/512/1045/1045140.png"
+        type="image/png"
+      />
 
-        <SpeedInsights/>
-        <Analytics/>
+      <body
+        className={cn("antialiased", fontHeading.variable, fontBody.variable)}
+      >
+        <SpeedInsights />
+        <Analytics />
         {children}
       </body>
     </html>
-  )
+  );
 }
